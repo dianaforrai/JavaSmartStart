53e9fc60917449007ad98f1b54cf1e6aea60c9cb
Diana Forrai Susana <diana.forrai12@yahoo.com>
2025-08-12 20:20:25 +0300

    Arrays - Demo

---
+++ Java Essentials/src/com/gl/app/model/Order.java
@@ -0,0 +1,8 @@
+package com.gl.app.model;
+
+public class Order {
+    public String getOrderId() {
+        String orderId;
+        return orderId;
+    }
+}
--- Java Essentials/src/com/gl/app/model/ShipmentOrder.java
+++ Java Essentials/src/com/gl/app/model/ShipmentOrder.java
@@ -6,7 +6,7 @@ enum Status {
     DELIVERED
 }
 
-class Order {
+public class ShipmentOrder {
     private String orderId;
     private String origin;
     private String destination;
@@ -14,7 +14,43 @@ class Order {
     private String customer_Name;
     private Status status;
 
-    public Order(String orderId, String origin, String destination, double weight, String customer_Name, Status status) {
+    public String getOrderId() {
+        return orderId;
+    }
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+    public String getOrigin() {
+        return origin;
+    }
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+    public String getDestination() {
+        return destination;
+    }
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+    public double getWeight() {
+        return weight;
+    }
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    public String getCustomer_Name() {
+        return customer_Name;
+    }
+    public void setCustomer_Name(String customer_Name) {
+        this.customer_Name = customer_Name;
+    }
+    public Status getStatus() {
+        return status;
+    }
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+    public ShipmentOrder(String orderId, String origin, String destination, double weight, String customer_Name, Status status) {
         this.orderId = orderId;
         this.origin = origin;
         this.destination = destination;
@@ -22,29 +58,26 @@ class Order {
         this.customer_Name = customer_Name;
         this.status = status;
     }
+    public static void main(String[] args) {
+        // declaration
+        ShipmentOrder[] shipmentOrders = new ShipmentOrder[2];
+        // initialization
+        shipmentOrders[0] = new ShipmentOrder("12345", "PUNE", "GOA", 10.0, "John Doe", Status.SHIPPED);
+        shipmentOrders[1] = new ShipmentOrder("67890", "BENGALURU", "GOA", 20.0, "Jane Doe", Status.SHIPPED);
 
-    public String getOrderId() {
-        return orderId;
+        // display the array values
+        displayArrayValues(shipmentOrders);
     }
 
-    public String getOrigin() {
-        return origin;
-    }
-
-    public String getDestination() {
-        return destination;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public String getCustomer_Name() {
-        return customer_Name;
-    }
-
-    public Status getStatus() {
-        return status;
+    public static void displayArrayValues(ShipmentOrder[] shipmentOrders) {
+        for (ShipmentOrder order : shipmentOrders) {
+            System.out.println("Order ID: " + order.getOrderId());
+            System.out.println("Origin: " + order.getOrigin());
+            System.out.println("Destination: " + order.getDestination());
+            System.out.println("Weight: " + order.getWeight() + " kg");
+            System.out.println("Customer Name: " + order.getCustomer_Name());
+            System.out.println("Status: " + order.getStatus());
+            System.out.println(); // Empty line for separation
+        }
     }
 }
-
