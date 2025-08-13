8a51d4d015ec2e1f88b02b7e0c26a33b7894c123
Diana Forrai Susana <diana.forrai12@yahoo.com>
2025-08-13 22:20:20 +0300

    Guest Class

--- Hilton Hotel Chain Application/src/Main.java
+++ Hilton Hotel Chain Application/src/Main.java
@@ -15,16 +15,81 @@ class Hotel {
         this.location = location;
     }
 
-    // Getters and Setters
-    public int getHotelId() { return hotelId; }
-    public void setHotelId(int hotelId) { this.hotelId = hotelId; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getLocation() { return location; }
-    public void setLocation(String location) { this.location = location; }
+    public int getHotelId() {
+        return hotelId;
+    }
+    public void setHotelId(int hotelId) {
+        this.hotelId = hotelId;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
     @Override
     public String toString() {
         return String.format("Hotel ID: %d | Name: %s | Location: %s", hotelId, name, location);
+    }
+}
+
+class Guest {
+    private int guestId;
+    private String name;
+    private String email;
+    private String phone;
+    private int hotelId;
+
+    public Guest(int guestId, String name, String email, String phone, int hotelId) {
+        this.guestId = guestId;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.hotelId = hotelId;
+    }
+
+    // Getters and Setters
+    public int getGuestId() {
+        return guestId;
+    }
+    public void setGuestId(int guestId) {
+        this.guestId = guestId;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public int getHotelId() {
+        return hotelId;
+    }
+    public void setHotelId(int hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Guest ID: %d | Name: %s | Email: %s | Phone: %s | Hotel ID: %d",
+                guestId, name, email, phone, hotelId);
     }
 }
