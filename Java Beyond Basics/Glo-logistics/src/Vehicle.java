2d2156b73c239f7e3934295b39238f546fe16fe2
Diana Forrai Susana <diana.forrai12@yahoo.com>
2025-08-20 15:40:48 +0300

    Vehicle

---
+++ Java Beyond Basics/Glo-logistics/src/Vehicle.java
@@ -0,0 +1,12 @@
+class Vehicle {
+    private String id;
+    private String type;
+    private double capacity;
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+    public double getCapacity() { return capacity; }
+    public void setCapacity(double capacity) { this.capacity = capacity; }
+}
