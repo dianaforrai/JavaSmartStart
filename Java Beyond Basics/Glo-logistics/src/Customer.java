4e6f2bc7c526fdd7d33286bb512a37f4fb7805b1
Diana Forrai Susana <diana.forrai12@yahoo.com>
2025-08-20 15:32:01 +0300

    Customer

---
+++ Java Beyond Basics/Glo-logistics/src/Customer.java
@@ -0,0 +1,10 @@
+class Customer {
+    private String name;
+    private Contact contactInfo;
+
+    // Getters and setters
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public Contact getContactInfo() { return contactInfo; }
+    public void setContactInfo(Contact contactInfo) { this.contactInfo = contactInfo; }
+}
