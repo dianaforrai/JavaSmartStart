b3ba24b004b39f6930f53656b89cccd54dc1e03b
Diana Forrai Susana <diana.forrai12@yahoo.com>
2025-08-20 15:39:24 +0300

    Status

---
+++ Java Beyond Basics/Glo-logistics/src/Status.java
@@ -0,0 +1,12 @@
+class Status {
+    private int code;
+    private String description;
+
+    public Status(int code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public int getCode() { return code; }
+    public String getDescription() { return description; }
+}
