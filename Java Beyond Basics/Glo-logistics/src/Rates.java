1afcc6ff74436e81e6c7ebfc7f5d9774cf6ee5aa
Diana Forrai Susana <diana.forrai12@yahoo.com>
2025-08-20 15:40:23 +0300

    Rates

---
+++ Java Beyond Basics/Glo-logistics/src/Rates.java
@@ -0,0 +1,12 @@
+class Rates {
+    private float ground;
+    private float air;
+    private float ocean;
+
+    public float getGround() { return ground; }
+    public void setGround(float ground) { this.ground = ground; }
+    public float getAir() { return air; }
+    public void setAir(float air) { this.air = air; }
+    public float getOcean() { return ocean; }
+    public void setOcean(float ocean) { this.ocean = ocean; }
+}
