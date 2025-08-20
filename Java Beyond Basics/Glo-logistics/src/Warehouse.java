aec299b8f2c7e246e5f39342b3bff658c714cc69
Diana Forrai Susana <diana.forrai12@yahoo.com>
2025-08-20 15:37:58 +0300

    Warehouse

---
+++ Java Beyond Basics/Glo-logistics/src/Warehouse.java
@@ -0,0 +1,21 @@
+class Warehouse {
+    private Location location;
+    private int capacity;
+    private List<Item> inventory;
+
+    public Location getLocation() { return location; }
+    public void setLocation(Location location) { this.location = location; }
+    public int getCapacity() { return capacity; }
+    public void setCapacity(int capacity) { this.capacity = capacity; }
+    public List<Item> getInventory() { return inventory; }
+    public void setInventory(List<Item> inventory) { this.inventory = inventory; }
+
+    public void addInventory(Item item) {
+        if (inventory == null) inventory = new ArrayList<>();
+        inventory.add(item);
+    }
+
+    public void removeInventory(Item item) {
+        if (inventory != null) inventory.remove(item);
+    }
+}
