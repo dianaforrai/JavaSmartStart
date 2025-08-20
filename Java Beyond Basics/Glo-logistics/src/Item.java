13d55d6b17164c4c6b924d3eacad4e8ca6b6e8fa
Diana Forrai Susana <diana.forrai12@yahoo.com>
2025-08-20 15:38:11 +0300

    Item

---
+++ Java Beyond Basics/Glo-logistics/src/Item.java
@@ -0,0 +1,15 @@
+class Item {
+    private String name;
+    private String description;
+    private double weight;
+
+    public Item(String name, String description, double weight) {
+        this.name = name;
+        this.description = description;
+        this.weight = weight;
+    }
+
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public double getWeight() { return weight; }
+}
