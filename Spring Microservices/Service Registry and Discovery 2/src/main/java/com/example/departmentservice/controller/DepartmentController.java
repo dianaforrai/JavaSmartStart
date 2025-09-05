0bf7164ad33141ccf0a1183ed82c840ca9258c37
Diana Forrai Susana <diana.forrai12@yahoo.com>
2025-09-05 16:27:50 +0300

    Service Registry and Discovery

---
+++ Spring Microservices/Service Registry and Discovery 2/src/main/java/com/example/departmentservice/controller/DepartmentController.java
@@ -0,0 +1,13 @@
+package com.example.departmentservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DepartmentController {
+
+    @GetMapping("/departments")
+    public String getDepartments() {
+        return "List of Departments";
+    }
+}
